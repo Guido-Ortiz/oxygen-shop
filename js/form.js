87f4389d3089fde6c92d81d8ac660d7624fc3aa4
Guido Ortiz <guidoortiz93@hotmail.com>
2023-02-03 12:11:20 +0100

    form & modal upd

--- js/form.js
+++ js/form.js
@@ -1,13 +1,77 @@
-const url = 'https://jsonplaceholder.typicode.com/guide/'
-var form = document.getElementById("form");
-var name = document.getElementById("name").value
-var email = document.getElementById("email").value
-console.log(name, email)
+// const url = 'https://jsonplaceholder.typicode.com/guide/'
+// var form = document.getElementById("form");
+// var name = document.getElementById("name").value
+// var email = document.getElementById("email").value
+// console.log(name, email)
 
-document.getElementById("btn-id").addEventListener("click", async function (e) {
-//   form.submit();
-e.preventDefault()
-  try{
+// document.getElementById("btn-id").addEventListener("click", async function (e) {
+// //   form.submit();
+// e.preventDefault()
+//   try{
+//     const result = await fetch(url, {
+//         method: 'POST',
+//         body: JSON.stringify({name, email
+//         //   Name: name ? name : null,
+//         //   Email:email,
+//         }),
+//         headers: {
+//           'Content-type': 'application/json; charset=UTF-8',
+//         },
+//       })
+//       const data = await result.json();
+//   }
+//   catch (e) {
+//     console.log(e)
+//   }
+
+// });
+
+
+
+
+
+// validation form
+const form = document.querySelector('.form--data');
+const inputs = document.querySelector('.input_text');
+const nameForm = document.querySelector('.name');
+const emailForm = document.querySelector('.email')
+const errorName = document.querySelector('.error_message_name');
+const errorEmail = document.querySelector('.error_message_email');
+const checkbox = document.querySelector('.checkbox')
+const policy = document.querySelector('.policy_label')
+const submitBtn = document.querySelector('.submit_button')
+const URL_FORM = "https://jsonplaceholder.typicode.com/guide/"
+
+const validateName = () => {
+    const regexName = /[a-zA-Z]{2,100}/
+    if (regexName.test(nameForm.value)) {
+        errorName.style.visibility = "hidden";
+        nameForm.classList.remove('error')
+        nameForm.classList.add("correct")
+    } else {
+        errorName.style.visibility = "visible";
+        nameForm.classList.remove('error')
+        nameForm.classList.add("correct")
+    }
+}
+const validateEmail = () => {
+    const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (regexMail.test(emailForm.value)) {
+        errorEmail.style.visibility = "hidden";
+        emailForm.classList.remove('error')
+        emailForm.classList.add("correct")
+    } else {
+        errorEmail.style.visibility = "visible";
+        emailForm.classList.remove('error')
+        emailForm.classList.add("correct")
+    }
+}
+nameForm.addEventListener('input', validateName)
+emailForm.addEventListener('input', validateEmail)
+
+async function sendForm(url,email, name) {
+    try{
     const result = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({name, email
@@ -23,90 +87,13 @@ e.preventDefault()
   catch (e) {
     console.log(e)
   }
+}
 
-});
-
-
-
-// const form = document.getElementById('form');
-// const nameForm = document.getElementById('name')
-// const emailForm = document.getElementById('email')
-// const checkbox = document.getElementById('checkbox')
-// const errorName = document.querySelector('.error_message_name');
-// const errorEmail = document.querySelector('.error_message_email');
-// const policy = document.querySelector('.policy_label')
-
-// const validateName = () => {
-//   const regexName = /[a-zA-Z]{2,100}/
-//   if (regexName.test(nameForm.value)) {
-//     errorName.style.visibility = "hidden";
-//     nameForm.classList.remove('error')
-//     nameForm.classList.add("correct")
-//   } else {
-//     errorName.style.visibility = "visible";
-//     nameForm.classList.remove('error')
-//     nameForm.classList.add("correct")
-//   }
-// }
-// const validateEmail = () => {
-//   const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-//   if (regexMail.test(emailForm.value)) {
-//     errorEmail.style.visibility = "hidden";
-//     emailForm.classList.remove('error')
-//     emailForm.classList.add("correct")
-//   } else {
-//     errorEmail.style.visibility = "visible";
-//     emailForm.classList.remove('error')
-//     emailForm.classList.add("correct")
-//   }
-// }
-// nameForm.addEventListener('input', validateName)
-// emailForm.addEventListener('input', validateEmail)
-
-// // async function sendForm(email, name,url) {
-// //   await fetch(url, {
-// //       method: "POST",
-// //       body: JSON.stringify({
-// //           email: email,
-// //           name: name,
-// //       }),
-// //       headers: {
-// //           'Content-type': 'application/json'
-// //       },
-// //   })
-// //       .then((response) => response.json())
-// //       .then((data)=> console.log(data))
-// // }
-
-// // form.addEventListener('submit', (e)=>{
-// //   e.preventDefault();
-// //   if(checkbox.checked){
-// //       policy.classList.remove('not_checked');
-// //       (nameForm.value&&emailForm.value)? sendForm(URL_FORM,nameForm.value,emailForm.value) : alert('complete all fields')
-// //   }else policy.classList.add("not_checked")
-
-// // })
-
-// document.getElementById("btn-id").addEventListener("click", async function (e) {
-//   //   form.submit();
-//   e.preventDefault()
-//   try {
-//     const result = await fetch(url, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         name, email
-//         //   Name: name ? name : null,
-//         //   Email:email,
-//       }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//       },
-//     })
-//     const data = await result.json();
-//   }
-//   catch (e) {
-//     console.log(e)
-//   }
-
-// });
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  if(checkbox.checked){
+      // policy.classList.remove('not_checked');
+      (nameForm.value&&emailForm.value)? sendForm(URL_FORM,nameForm.value,emailForm.value) : alert('complete all fields')
+  }else alert('checked terms and conditions')
+  // policy.classList.add("not_checked")
+})
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,7 @@
 // MODAL
 var modal = document.getElementById("ventanaModal");
 
-
-// Botón que abre el modal
-// var boton = document.getElementById("abrirModal");
-
-// Hace referencia al elemento <span> que tiene la X que cierra la ventana
 var span = document.getElementsByClassName("cerrar")[0];
-
-// Cuando el usuario hace click en el botón, se abre la ventana
-// boton.addEventListener("click", function () {
-//     modal.style.display = "block";
-// });
 
 // Si el usuario hace click en la x, la ventana se cierra
 span.addEventListener("click", function () {
@@ -25,33 +15,21 @@ window.addEventListener("click", function (event) {
     }
 });
 
-var flag = false;
-// console.log(flag)
-
-// Abre el modal luego de un scroll del 25%
-document.addEventListener("scroll", () => {
-    const scrolledPercentage =
-        (scrollContainer().scrollTop /
-            (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-        100;
-
-        // console.log(Math.round(scrolledPercentage))
-
-        // console.log(flag)
-        if(Math.round(scrolledPercentage) === 25 && flag === false){
-            modal.style.display = "block";
-            flag = true
-        }
-});
-
-// Abre el modal luego de 6 seg
-function displayModal() { 
-    if(flag === false){
+const modalActive = true
+setTimeout(()=>{
+    if(sessionStorage.getItem('modalActive')!== '1'){
         modal.style.display = "block";
-        flag = true
-        // console.log(flag)
-    } 
-    // console.log(flag)  
-}
-setInterval(displayModal, 6000);
+        sessionStorage.setItem('modalActive','1')
+    }
+}, 6000)
 
+const showModal = () =>{
+modalActive = false
+}
+document.addEventListener("ev",showModal)
+document.addEventListener("scroll", ()=> {
+    if (sessionStorage.getItem("modalActive") !== "1" && (scrollY/(document.body.offsetHeight - innerHeight))*100 >= 25) {
+    modal.style.display = "block";
+    sessionStorage.setItem("modalActive", "1");
+    }
+})
